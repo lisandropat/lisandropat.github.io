58c0390f15362dd382f879f40fed0f7491190c3a
Lisandro <lisandropat@gmail.com>
2025-07-12 15:12:54 +0200

    Update Shot.jsx

--- src/components/Shots/Shot.jsx
+++ src/components/Shots/Shot.jsx
@@ -8,18 +8,12 @@ import {
 
 const Shot = ({shotDescription, shotImage}) => (
     <Box>
-        <Box
-            padding={{ base: "12px", lg: "20px" }}
-            borderRadius="36px"
-            border="0.5px solid rgba(0,0,0,0.1)"
-        >
-            <Image 
-                src={shotImage}
-                borderRadius="24px"
-                height={{ base: "60vh", lg: "80vh" }}
-                width="auto"
-            />
-        </Box>
+        <Image 
+            src={shotImage}
+            borderRadius="24px"
+            height={{ base: "60vh", lg: "80vh" }}
+            width="auto"
+        />
         <Text fontSize="14px" mt="21px">{shotDescription}</Text>
     </Box>
 )
